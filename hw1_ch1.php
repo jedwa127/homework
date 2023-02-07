06d9bbb13400f4f110246c24798d57b4bb4f1105
jedwa127 <124191325+jedwa127@users.noreply.github.com>
2023-02-07 17:46:01 -0500

    INFO-638 Code Homework 1

---
+++ hw1_ch1.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Homework 1, Challenge 1: Correct Change</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<?php
+			$change = 786.5; // Amount of change due back
+			echo "You are due back $change cents in change.<br>";
+				if ($change < 0) { // Checks if $change is a negative number
+					exit("You can't recieve negative change, that's just paying someone I guess."); // Exit program and print if $change is a negative number
+				}
+				if (intval($change) != $change) { // Checks if $change is an integer or a floating point number by comparing integer value of $change with original value of $change
+					echo "There aren't any coins worth less than one cent anymore so it looks like you're getting stiffed.<br>"; // Prints if $change is a floating point number
+				}
+				else {
+					echo ""; // Prints if $change is an integer
+				}
+			echo "You will recieve:<br>";			
+			$change = (int) $change; // Sets $change to integer value of original input to sanitize floating-point change numbers
+			$dollars = $change / 100; // Calculates amount divisible by 100
+			$dollars = (int) $dollars; // Takes only integer from previous calculation
+				if ($dollars > 1) { // Correctly pluralizes denomination or skips printing if none of that denomination is received
+					echo $dollars . " dollars.<br>";
+				}
+				elseif ($dollars < 1) {
+					echo "";
+				}
+				else {
+					echo $dollars . " dollar.<br>";
+				}
+			$quarters = ($change - ($dollars * 100)) / 25; // Calculates remainder divisible by 25
+			$quarters = (int) $quarters; // Takes only integer from previous calculation
+				if ($quarters > 1) { // Correctly pluralizes denomination or skips printing if none of that denomination is received
+					echo $quarters . " quarters.<br>";
+				}
+				elseif ($quarters < 1) {
+					echo "";
+				}
+				else {
+					echo $quarters . " quarter.<br>";
+				}
+			$dimes = ($change - (($dollars * 100) + ($quarters * 25))) / 10; // Calculates remainder divisible by 10
+			$dimes = (int) $dimes; // Takes only integer from previous calculation
+				if ($dimes > 1) { // Correctly pluralizes denomination or skips printing if none of that denomination is received
+					echo $dimes . " dimes.<br>";
+				}
+				elseif ($dimes < 1) {
+					echo "";
+				}
+				else {
+					echo $dimes . " dime.<br>";
+				}
+			$nickels = ($change - (($dollars * 100) + ($quarters * 25) + ($dimes * 10))) / 5; // Calculates remainder divisible by 5
+			$nickels = (int) $nickels; // Takes only integer from previous calculation
+				if ($nickels > 1) { // Correctly pluralizes denomination or skips printing if none of that denomination is received
+					echo $nickels . " nickels.<br>";
+				}
+				elseif ($nickels < 1) {
+					echo "";
+				}
+				else {
+					echo $nickels . " nickel.<br>";
+				}
+			$pennies = $change - (($dollars * 100) + ($quarters * 25) + ($dimes * 10) + ($nickels *5)); // Calculates remainder
+				if ($pennies > 1) { // Correctly pluralizes denomination or skips printing if none of that denomination is received
+					echo $pennies . " pennies.";
+				}
+				elseif ($pennies < 1) {
+					echo "";
+				}
+				else {
+					echo $pennies . " penny.";
+				}
+		?>
+	</body>
+</html>
---
+++ hw1_ch2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Homework 1, Challenge 2: 99 Bottles of Beer</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<?php
+			for ($bottles = 99 ; $bottles >= 1 ; $bottles) { // Changing value of $bottles will change number of song verses accordingly
+				if ($bottles > 1) {
+					echo $bottles . " bottles of beer on the wall, " . $bottles . " bottles of beer.<br>Take one down, pass it around, " . --$bottles . " bottles of beer on the wall.<br>"; // Decrementing $bottles at this location as opposed to in the modification statement of the for loops prints lyrics correctly
+				}
+				elseif ($bottles = 1) {
+					echo $bottles . " bottles of beer on the wall, " . $bottles . " bottles of beer.<br>Take one down, pass it around, no more bottles of beer on the wall."; // Prints final line of song differently
+					break; // Terminate program to prevent infinite loop at $bottles value of 1
+				}
+			}
+		?>
+	</body>
+</html>
